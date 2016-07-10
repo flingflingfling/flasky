d75a8d7fc3d55802ee0e988fad7cd4dc9f6e5c43
garryforgit <fly@purplemapledb.maple>
2016-07-10 17:06:46 +0800

    update Readme,add flask_scrpt support

--- README.md
+++ README.md
@@ -3,4 +3,22 @@ Demo with the flasy tutorial(book).
 
 Flask is a python web microframwork.this is the demo whit flask in python.
 
+## about database
+I am a new guy to the coding,so i will follow the introduce.
+Here i am using sqlite3 as the database.
+
+# about db-migration
+This project will use flask_script to support alembic.
+
+alembic could make the database become easy to migration.
+
+###the code like that:
+>python hello.py db migrate -m "initial migration"
+
+upgrade command:
+>python hello.py db upgrade
+
+downgrade command:
+>python hello.py db downgrade 
+
 
--- app/hello.py
+++ app/hello.py
@@ -1,22 +1,72 @@
 # coding:utf-8
 
+import os.path
 from flask import Flask, render_template, session, \
     redirect, url_for, flash
-from flask_script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask_script import Manager, Shell     # add shell command support
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment     # add localtime support
 from datetime import datetime  # print the local time
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, SubmitField \
     # get the text data and commit button data .
 from wtforms.validators import Required \
     # Required validata the data is not null
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand   # add db-migration support
 
+
+
+
+# initial app
 app = Flask(__name__)
+
+# database settings; using flask_sqlalchemy with mysql
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    "sqlite:///" + os.path.join(basedir, 'flasky-sqlalchemy-sqlite3.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'do not you trust me '
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role',lazy='dynamic')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # view function
@@ -24,14 +74,20 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Things like you have changed your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))  # url_for must get 1
         # arguments, it is view function,here is index()
-    return render_template('index.html', \
-                           form=form, name=session.get('name'))
+    return render_template('index.html', form=form, \
+                name=session.get('name'), known = session.get('known',False),
+                current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -55,7 +111,11 @@ class NameForm(Form):
     # SubmitField is > type="submit"的,<input>元素
     submit = SubmitField('Submit')
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)   # add db-migrate command
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # manager.run()
+    #app.run(debug=True)
+    manager.run()
