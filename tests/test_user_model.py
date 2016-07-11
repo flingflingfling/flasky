17f37bb9ad68d655112412139e3b9115d08b5709
garryforgit <fly@purplemapledb.maple>
2016-07-11 22:18:33 +0800

    delete the unuseful file,track the db file,you can check it with sqlitebrowser

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,9 @@
 # coding:utf8
 
 import unittest
+import time
 from app.models import User
+from app import create_app, db
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -23,3 +25,26 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='ohh')
         u2 = User(password='doge')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='ohh')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.ocnfirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='oach')
+        u2 = User(password='doge')
+        db.session.add_all([u1,u2])
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='ohh')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
